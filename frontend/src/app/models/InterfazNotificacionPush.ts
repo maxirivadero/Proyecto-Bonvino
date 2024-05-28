140b731576112bbdab2e3ea608d22d6b724ea095
maxirivadero <maxirivadero2000@gmail.com>
2024-05-28 03:55:27 -0300

    funka la mitad

--- frontend/src/app/models/InterfazNotificacionPush.ts
+++ frontend/src/app/models/InterfazNotificacionPush.ts
@@ -1,3 +1,17 @@
+import usuarios from '../../assets/json/usuarios'; // Importar el array de usuarios
+
 export class InterfazNotificacionPush {
-    
+    actualizarNovedadBodega(arregloEnofilos: Array<string>) {
+        for (const nombreEnofilo of arregloEnofilos) {
+            // Buscar el nombre del enófilo en el array de usuarios
+            const usuario = usuarios.find(usuario => usuario.nombre === nombreEnofilo);
+            
+            // Si se encuentra el usuario, imprimir un mensaje en la consola
+            if (usuario) {
+                console.log(`Se mandó una notificación a ${nombreEnofilo}`);
+            } else {
+                console.log(`No se encontró un usuario con el nombre ${nombreEnofilo}`);
+            }
+        }
+    }
 }
