bc9d589b5acf76ed032f7a0389b82ecf91359a2c
maxirivadero <maxirivadero2000@gmail.com>
2024-05-31 22:43:54 -0300

    actualize develop

---
+++ frontend/src/app/feature/pantalla-actualizacion/pantalla-actualizacion/pantalla-actualizacion.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { Bodega } from 'src/app/models/Bodega';
+import { GestorActualizacion } from 'src/app/models/GestorActualizacion';
+
+@Component({
+  selector: 'app-pantalla-actualizacion',
+  templateUrl: './pantalla-actualizacion.component.html',
+  styleUrls: ['./pantalla-actualizacion.component.scss']
+})
+export class PantallaActualizacionComponent {
+  gestor = new GestorActualizacion();
+  habilitarVentanaFlag = true;
+  comboBodegasActualizables = false;
+  mostrarOpcionesDeBodega = false;
+  bodegaSeleccionada = new Bodega(
+    "Bodega La Riña",
+    "Una bodega familiar fundada en 1975, especializada en vinos tintos de alta calidad.",
+    "La historia de Bodega La Viña se remonta a hace casi medio siglo, cuando el fundador, Don Alejandro, plantó las primeras vides en estas tierras. Desde entonces, la bodega ha pasado de generación en generación, manteniendo la tradición y el compromiso con la calidad.",
+    [42.8782, -8.5448],
+    2,
+    new Date("2024-03-15T12:30:00.000Z")
+    );
+
+  opcionImportarActualizacionDeVinos() {
+    this.gestor.importarActualizacionDeVinos();
+    this.habilitarVentana();
+    console.log(this.gestor.bodegasActualizables);
+    this.mostrarBodegasDisponibles();
+  };
+  habilitarVentana() {
+    this.habilitarVentanaFlag = !this.habilitarVentanaFlag;
+  };
+  mostrarBodegasDisponibles() {
+    if(this.gestor.bodegasActualizables && this.gestor.bodegasActualizables.length > 0){
+      this.comboBodegasActualizables = !this.comboBodegasActualizables;
+    }
+  };
+  solicitarSeleccionBodega() {
+    if(this.gestor.bodegasActualizables && this.gestor.bodegasActualizables.length > 0){
+      this.mostrarOpcionesDeBodega = !this.mostrarOpcionesDeBodega;
+    }
+  }
+
+  tomarSeleccionBodega(bodega: Bodega) {
+    this.bodegaSeleccionada = bodega;
+    this.gestor.tomarSeleccionBodega(bodega);
+    this.comboBodegasActualizables = !this.comboBodegasActualizables;
+    this.mostrarOpcionesDeBodega = !this.mostrarOpcionesDeBodega;
+    this.mostrarResumenActualizacion();
+  }
+  mostrarResumenActualizacion() {
+    console.log(this.gestor.vinosActualizados);
+    this.gestor.notificarSubscripciones(this.bodegaSeleccionada);
+  };
+}
